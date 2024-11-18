fee9d4cfeea0aada0289aa1140638d0dd6d309b2
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2024-11-18 18:38:34 +0200

    fix: fundamentals; basic programming model; exercise; binomial distribution added question

--- Fundamentals/Basic_Programming_Model/Exercises/Creative_Problems/BinomialDistribution.java
+++ Fundamentals/Basic_Programming_Model/Exercises/Creative_Problems/BinomialDistribution.java
@@ -1,3 +1,13 @@
+// 1.1.27 Binomial distribution. Estimate the number of recursive calls that would be
+// used by the code
+// public static double binomial(int N, int k, double p)
+// {
+//  if ((N == 0) || (k < 0)) return 1.0;
+//  return (1.0 - p)*binomial(N-1, k) + p*binomial(N-1, k-1);
+// }
+// to compute binomial(100, 50). Develop a better implementation that is based on
+// saving computed values in an array.
+
 public class BinomialDistribution {
 
     static int count = 0;
